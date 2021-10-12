046c6d3b97a7c2d9c0e9615714cf47d5b41325f4
imtheu <matheusfrmorais@hotmail.com>
2021-10-12 13:54:35 -0300

    Fix build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const withMDX = require('@next/mdx')({
 	extension: /\.mdx$/
 });
 
+if (
+	process.env.LD_LIBRARY_PATH == null ||
+	!process.env.LD_LIBRARY_PATH.includes(
+		`${process.env.PWD}/node_modules/canvas/build/Release:`
+	)
+) {
+	process.env.LD_LIBRARY_PATH = `${
+		process.env.PWD
+	}/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+
 /** @type {import('next').NextConfig} */
 module.exports = withMDX({
 	i18n: {
