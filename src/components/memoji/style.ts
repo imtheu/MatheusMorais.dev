7e4600e411548a341a5fb609718a19001691e23a
imtheu <matheusfrmorais@hotmail.com>
2022-04-24 21:11:17 -0300

    fix: fix animation on ios

--- src/components/memoji/style.ts
+++ src/components/memoji/style.ts
@@ -3,35 +3,50 @@ import { keyframes } from '@stitches/react';
 import { styled } from 'style/stitches.config';
 
 const huhuZoomInRight = keyframes({
-	'50%': { transform: 'translateY(5px) translateX(4px)' }
+	'0%': { transform: 'translateX(0px) translateY(0px)' },
+	'100%': { transform: 'translateX(4px) translateY(5px)' }
 });
 
 const huhuZoomInLeft = keyframes({
-	'50%': { transform: 'translateY(-5px) translateX(-2px)' }
+	'0%': { transform: 'translateX(0px) translateY(0px)' },
+	'100%': { transform: 'translateX(-1px) translateY(-5px)' }
 });
 
 const huhuFadeOut = keyframes({
-	'50%': { transform: 'translateX(-4px)' }
+	'0%': { transform: 'translateX(0px)' },
+	'100%': { transform: 'translateX(-4px)' }
 });
 
 const huhuZoomInUp = keyframes({
-	'50%': { transform: 'translateX(6px)' }
+	'0%': { transform: 'translateX(0px)' },
+	'100%': { transform: 'translateX(6px)' }
 });
 
 export const Memoji = styled('div', {
 	textAlign: 'center',
+	position: 'relative',
+
+	'.logoAnimation': {
+		animationIterationCount: 'infinite',
+		animationDirection: 'alternate',
+		animationTimingFunction: 'ease-in-out'
+	},
 
 	'.logoAnimation[data-aos="zoom-in-right"], .logoAnimation[data-aos="zoom-in"]':
 		{
-			animation: `${huhuZoomInRight} infinite 4s ease-in-out`
+			animationName: `${huhuZoomInRight}`,
+			animationDuration: '2.5s'
 		},
 	'.logoAnimation[data-aos="zoom-in-left"]': {
-		animation: `${huhuZoomInLeft} infinite 3s ease-in-out`
+		animationName: `${huhuZoomInLeft}`,
+		animationDuration: '1.5s'
 	},
 	'.logoAnimation[data-aos="fade-out"]': {
-		animation: `${huhuFadeOut} infinite 2.5s ease-in-out`
+		animationName: `${huhuFadeOut}`,
+		animationDuration: '2s'
 	},
 	'.logoAnimation[data-aos="zoom-in-up"]': {
-		animation: `${huhuZoomInUp} infinite 3s ease-in-out`
+		animationName: `${huhuZoomInUp}`,
+		animationDuration: '2.8s'
 	}
 });
